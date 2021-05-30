794a19a3e0b5357204ab762a6874e308235de7aa
Bohemian-hub <dongshangwl@gmail.com>
2021-05-30 20:47:02 +0800

    提交今天的Java代碼

--- Test1/src/com/xc/dao/UserinfoDao.java
+++ Test1/src/com/xc/dao/UserinfoDao.java
@@ -2,30 +2,88 @@ package com.xc.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xc.entity.Userinfo;
 
 public class UserinfoDao {
-	public void deleteUserInfo(String ac) {
 
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				String url ="jdbc:mysql://127.0.0.1:3306/hd";
-				String user="root";
-				String password = "1234";
-				Connection conn = DriverManager.getConnection(url,user,password);
+	public ResultSet selectUserInfo(Userinfo userinfo) throws ClassNotFoundException, SQLException {
+		ResultSet rs = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://127.0.0.1:3306/hd";
+		String user = "root";
+		String password = "1234";
+		Connection conn = DriverManager.getConnection(url, user, password);
+		Statement st = conn.createStatement();
+		String sql = "select * from userinfo where account = '" + userinfo.account + "' and password = '"+userinfo.password+"'";
+		rs = st.executeQuery(sql);
+		return rs;
 
-				Statement st =conn.createStatement();
-				String sql = "delete from userinfo where account = '"+ac+"'";
-				st.execute(sql);
-				
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	}
+
+	public List<Userinfo> selectUserInfoAll() throws ClassNotFoundException, SQLException {
+		List<Userinfo> list = new ArrayList<Userinfo>();
+		ResultSet rs = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://127.0.0.1:3306/hd";
+		String user = "root";
+		String password = "1234";
+		Connection conn = DriverManager.getConnection(url, user, password);
+		Statement st = conn.createStatement();
+		String sql = "select * from userinfo";
+		ResultSet rs2 = st.executeQuery(sql);
+       
+        while(rs2.next()){//循环一次是一行
+        	Userinfo userinfo = new Userinfo();
+        	userinfo.username = rs2.getString("username");
+        	userinfo.account = rs2.getString("account");
+        	userinfo.password = rs2.getString("password");
+        	list.add(userinfo);
+        }
+        return list;
+
+	}
+
+	public void insertUserInfo(Userinfo userinfo) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://127.0.0.1:3306/hd";
+		String user = "root";
+		String password = "1234";
+		Connection conn = DriverManager.getConnection(url, user, password);
+		Statement st = conn.createStatement();
+		String sql = "insert into userinfo values('" + userinfo.account + "','" + userinfo.password + "','"
+				+ userinfo.username + "')";
+		st.execute(sql);
+	}
+
+	public void updateUserInfo(Userinfo userinfo) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://127.0.0.1:3306/hd";
+		String user = "root";
+		String password = "1234";
+		Connection conn = DriverManager.getConnection(url, user, password);
+		Statement st = conn.createStatement();
+		String sql = "update userinfo set username = '" + userinfo.username + "'," + "password = '" + userinfo.password
+				+ "' where account = '" + userinfo.account + "'";
+		st.execute(sql);
+	}
+
+	public void deleteUserInfo(String ac) throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://127.0.0.1:3306/hd";
+		String user = "root";
+		String password = "1234";
+		Connection conn = DriverManager.getConnection(url, user, password);
+
+		Statement st = conn.createStatement();
+		String sql = "delete from userinfo where account = '" + ac + "'";
+		st.execute(sql);
 
 	}
 }
--- Test1/src/com/xc/test/Test3.java
+++ Test1/src/com/xc/test/Test3.java
@@ -1,12 +1,40 @@
 package com.xc.test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import com.xc.dao.UserinfoDao;
+import com.xc.entity.Userinfo;
 
 public class Test3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		UserinfoDao da = new UserinfoDao();
-				da.deleteUserInfo("ls");
+		// da.deleteUserInfo("ls");
+//		Userinfo userinfo = new Userinfo();
+//		userinfo.account = "ls";
+//		userinfo.username = "ÀîËÄ¹â";
+//		userinfo.password = "666";
+//		da.updateUserInfo(userinfo);
 		
+		
+		Userinfo userinfo = new Userinfo();
+		userinfo.account = "hq";
+		userinfo.username = "Øi°Ë½ä";
+		userinfo.password = "666";
+		ResultSet rs =  da.selectUserInfo(userinfo);
+		System.out.println(rs.next());
+		
+		
+
+        
+         
+//        List<Userinfo> list=da.selectUserInfoAll();
+//        for(Userinfo u:list) {
+//        	System.out.println(u.account+u.username+u.password);
+//        }
+        
+
 	}
 }
